b093e84c6ee2f3b89d34dd164cf7d5095e798e34
qaqRose <2309505683@qq.com>
2019-09-29 02:19:20 +0000

    Deploying to gh-pages from master b95ad7057a0d47145a53d612d7bea0c3560c899f

---
+++ precache-manifest.d631c9fae28530396e85b8a75a81832f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f78e79ad08bd750d54c0dae7a5d39f",
+    "url": "/github_action_test_demo/index.html"
+  },
+  {
+    "revision": "db1395fb0f150c4adf2c",
+    "url": "/github_action_test_demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "13c9baf672cfb88d7f43",
+    "url": "/github_action_test_demo/static/js/2.a84ab55e.chunk.js"
+  },
+  {
+    "revision": "db1395fb0f150c4adf2c",
+    "url": "/github_action_test_demo/static/js/main.1e693c20.chunk.js"
+  },
+  {
+    "revision": "441ed857c08ac18f998c",
+    "url": "/github_action_test_demo/static/js/runtime~main.d5314414.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github_action_test_demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
